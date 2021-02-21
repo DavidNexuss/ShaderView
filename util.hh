ce937a578e849f9efd86ce5d0ffa651bccacbbfe
davidnexus <dgtgar01@gmail.com>
2021-02-21 11:50:50 +0100

    Removes unused glm dependency

--- util.hh
+++ util.hh
@@ -1,7 +1,8 @@
 #pragma once
+#define GLFW_STATIC
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
 
 GLuint LoadShaders(const char * fragment_file_path);
 GLuint loadTexture(const char * texture_file_path);
